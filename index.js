3ae15eccd7fe7cc2b6eb180db8b77629bfa1a5e7
ofins <jkyeewang@gmail.com>
2023-12-30 22:07:39 +0800

    test fetchData

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ import fetch from "node-fetch";
 export const fetchData = async (url) => {
   console.log("url in fetchData:", url);
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        method:'GET',
+        headers:{
+            Accept: 'application/json',
+            'Content-Type':'application/json'
+        }
+    });
+    console.log('response', response)
     const data = await response.json();
-
+    console.log('data', data)
     return { status: response.status, data };
   } catch (error) {
     throw new Error(`Error in fetchData: ${error.message}`);
